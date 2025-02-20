142098766c8b5ecc84086609cc8d79565224d4d5
Chad Shenk <chad.shenk@flibe-energy.com>
2025-02-19 21:18:12 -0600

    background pulse

---
+++ src/index.js
@@ -0,0 +1,10 @@
+(function () {
+    window.addEventListener("DOMContentLoaded", (event) => {
+        init();
+    });
+
+    function init() {
+        console.log("Loaded application script!");
+    }
+
+})();
